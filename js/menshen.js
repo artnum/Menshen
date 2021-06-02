345fa8f6ed4f0bdc8331eb6a13291d611d56be37
artnum <etienne@artisan-numerique.ch>
2021-06-02 15:35:36 +0200

    Initial commit
    Basic Menshen idea, header authentication, query string authentication

---
+++ js/menshen.js
@@ -0,0 +1,536 @@
+/* eslint-env browser, worker */
+const MenshenEncoding = {
+  base64Decode: (t) => {
+    try {
+      const s = atob(t.trim().replace(/\r?\n|\r/g, ''))
+      const b = new ArrayBuffer(s.length)
+      const v = new Uint8Array(b)
+      for (let i = 0; i < s.length; i++) {
+        v[i] = s.charCodeAt(i)
+      }
+      return b
+    } catch (e) {
+      return null
+    }
+  },
+
+  base64Encode: (b) => {
+    try {
+      let t = ''
+      const s = btoa(String.fromCharCode.apply(null, new Uint8Array(b)))
+      for (let i = 0; i < (Math.ceil(s.length / 64)); i++) {
+        t += s.substring(i * 64, (i + 1) * 64) + '\n'
+      }
+      return t
+    } catch (e) {
+      return null
+    }
+  },
+
+  hex2buf: (text) => {
+    let buf = new Uint8Array(text.length / 2)
+    let x = 0
+    for (let i = 0; i < text.length; i++) {
+      switch (text[i]) {
+        case '0': break
+        case '1': x |= 1; break
+        case '2': x |= 2; break
+        case '3': x |= 3; break
+        case '4': x |= 4; break
+        case '5': x |= 5; break
+        case '6': x |= 6; break
+        case '7': x |= 7; break
+        case '8': x |= 8; break
+        case '9': x |= 9; break
+        case 'A': case 'a': x |= 0x0a; break
+        case 'B': case 'b': x |= 0x0b; break
+        case 'C': case 'c': x |= 0x0c; break
+        case 'D': case 'd': x |= 0x0d; break
+        case 'E': case 'e': x |= 0x0e; break
+        case 'F': case 'f': x |= 0x0f; break
+        default: return null
+      }
+      if ((i + 1) % 2 === 0) {
+        buf[i >> 1] = x
+        x = 0
+      }
+      x <<= 4
+    }
+    return buf
+  },
+
+  buf2hex: (buffer) => {
+    return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
+  },
+  
+  btoa: (value) => {
+    return btoa(value).replaceAll('+', '-').replaceAll('/', '_').replaceAll('=', '.')
+  },
+
+  buf2b64: (buffer) => {
+    var binary = ''
+    var bytes = new Uint8Array( buffer )
+    var len = bytes.byteLength
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode( bytes[ i ] )
+    }
+    return MenshenEncoding.btoa( binary )
+  },
+
+  readPEM: (c) => {
+    const pemRegex = /^\s*-----BEGIN (PRIVATE|PUBLIC) KEY-----$([a-zA-Z0-9/+=\r\n]*)^-----END (?:PRIVATE|PUBLIC) KEY-----\s*$/mg
+    const m = pemRegex.exec(c)
+    if (m && m.length > 1) {
+      return {cert: MenshenEncoding.base64Decode(m[2]), privkey: m[1] === 'PRIVATE'}
+    }
+  },
+
+  writePEM: (c, p = false) => {
+    let t = `-----BEGIN ${p ? 'PRIVATE' : 'PUBLIC'} KEY-----\n`
+    t += MenshenEncoding.base64Encode(c)
+    return t + `-----END ${p ? 'PRIVATE' : 'PUBLIC'} KEY-----\n`
+  }
+}
+
+const MenshenRSAUtils = {
+  sign: (privkey, text, options = {saltLength: 0, hash: 'SHA-256', raw: false}) => {
+    return new Promise((resolve, reject) => {
+      if (!privkey) { resolve(false); return }
+      crypto.subtle.sign(
+        {
+          name: 'RSA-PSS',
+          saltLength: options.saltLength,
+          hash: {
+            name: options.hash
+          }
+        },
+        privkey,
+        (new TextEncoder()).encode(text))
+        .then((signed) => {
+          if (options.raw) {
+            resolve(signed)
+          } else {
+            resolve(MenshenEncoding.base64Encode(signed))
+          }
+        })
+    })
+  },
+
+  verify: (pubkey, text, sign, options = {raw: false, hash: 'SHA-256', saltLength: 0}) => {
+    return new Promise((resolve, reject) => {
+      if (!pubkey) { resolve(false); return }
+      if (!options.raw) {
+        sign = MenshenEncoding.base64Decode(sign)
+      }
+      if (!sign) { resolve(false); return }
+      crypto.subtle.verify(
+        {
+          name: 'RSA-PSS',
+          saltLength:
+          options.saltLength,
+          hash: {
+            name: options.hash
+          }
+        },
+        pubkey,
+        sign,
+        (new TextEncoder()).encode(text)
+      )
+      .then((v) => { resolve(v) })
+    })
+  }
+}
+
+function MenshenKeyStore () {
+  this.IDB = null
+}
+
+MenshenKeyStore.prototype.init = function () {
+  this._init = new Promise((resolve, reject) => {
+      const request = indexedDB.open('MenshenKeyStore', 1)
+      request.onupgradeneeded = (event) => {
+          const idb = event.target.result
+          if (!idb.objectStoreNames.contains('keystore')) {
+            idb.onsuccess = (event) => {
+              resolve(idb)
+            }
+            idb.createObjectStore('keystore', {keyPath: 'k'})
+          } else {
+            resolve(idb)
+          }
+      }
+
+      request.onsuccess = (event) => {
+          resolve(event.target.result)
+      }
+  })
+  return this._init
+}
+
+MenshenKeyStore.prototype.eraseAuth = function (nick) {
+  return new Promise((resolve, reject) => {
+      this.init()
+      .then(idb => {
+        const transaction = idb.transaction('keystore', 'readwrite')
+        const request = transaction.objectStore('keystore').clear()
+        request.onerror = (event) => { reject(event.message) }
+        request.onabort = (event) => { resolve() }
+        request.oncomplet = (event) => { resolve() }
+      })
+  })
+}
+
+MenshenKeyStore.prototype.getAuth = function () {
+  return new Promise((resolve, reject) => {
+    this.init()
+    .then(idb => {
+      const transaction = idb.transaction('keystore', 'readonly')
+      const request = transaction.objectStore('keystore').get('current')
+      request.onsuccess = event => resolve(event.target.result)
+      request.onerror = event => reject(event.target.error)
+      transaction.onabort = event => resolve()
+    })
+  })
+}
+
+MenshenKeyStore.prototype.importPrivateKey = function (PEMKey, clientId, nickName, hash, opts = {}) {
+  return new Promise((resolve, reject) => {
+    const key = MenshenEncoding.readPEM(PEMKey)
+    if (!key) { reject(new Error('Cannot read PEM private key')); return }
+    if (!key.privkey) { reject(new Error('Key is not private')); return }
+
+    crypto.subtle.importKey(
+      'pkcs8',
+      key.cert,
+      { name: 'RSA-PSS', hash: { name: hash } },
+      false,
+      ['sign']
+    )
+    .then((k) => {
+      return this.storeAuth(nickName, clientId, k, opts)
+    })
+    .then(() => {
+      resolve()
+    })
+    .catch(reason => {
+      reject(reason)
+    })
+  })
+}
+
+MenshenKeyStore.prototype.storeAuth = function (nick, cid, pkey, opts = {}) {
+  return new Promise((resolve, reject) => {
+      if (!pkey instanceof CryptoKey) {
+          reject('Not a cryptokey')
+          return
+      }
+    this.init()
+    .then(idb => {
+      const transaction = idb.transaction('keystore', 'readwrite')
+
+      transaction.onerror = event => reject(new Error(event.message))
+      transaction.onabort = event => resolve()
+      transaction.oncomplete = event => { resolve() }
+
+      transaction.objectStore('keystore').put({
+          k: 'current',
+          nick: nick,
+          cid: cid,
+          pkey: pkey,
+          opts: opts
+      })
+    })
+  })
+}
+
+const GLOBAL = new Function('return this')()
+
+function Menshen (options = {}, _fetch = null) {
+  this.version = 1
+  if (options.version === 2) {
+    this.version = 2
+  }
+  if (_fetch !== null) {
+    this.fetchFunction = _fetch
+  } else {
+    this.fetchFunction = GLOBAL.fetch
+  }
+  this.kloaded = false
+  this.saltLength = 0
+  this.hash = 'SHA-256'
+  this.key = { priv: null, pub: null }
+  if (options.privkey) {
+    this.loadKey(options.privkey)
+  }
+  if (options.pubkey) {
+    this.loadKey(options.pubkey)
+  }
+  if (options.hash) {
+    this.setHash(options.hash)
+  }
+  if (options.salt) {
+    this.setSaltLength(options.salt)
+  }
+}
+
+Menshen.prototype.isLoaded = function () {
+  if (this._isLoaded === undefined) {
+    return new Promise((resolve, reject) => { reject(new Error('No attempt to load key done')) })
+  }
+  return this._isLoaded
+}
+
+Menshen.prototype.loadKey = function (txt) {
+  return new Promise((resolve, reject) => {
+    let k = MenshenEncoding.readPEM(txt)
+    if (k) {
+      if (k.privkey) {
+        crypto.subtle.importKey(
+          'pkcs8',
+          k.cert,
+          { name: 'RSA-PSS', hash: { name: this.hash } },
+          false,
+          ['sign']
+        )
+        .then(
+          (k) => {
+            this.key.priv = k
+            this.kloaded = true
+            resolve(this.key.priv)
+          }
+        )
+      } else {
+        crypto.subtle.importKey(
+          'spki',
+          k.cert,
+          { name: 'RSA-PSS', hash: { name: this.hash } },
+          false,
+          ['verify']
+        )
+        .then(
+          (k) => {
+            this.key.pub = k
+            this.kloaded = true
+            resolve(this.key.pub)
+          }
+        )
+      }
+    }
+  })
+}
+
+Menshen.prototype.setClientId = function (cid) {
+  this.clientid = cid
+}
+
+Menshen.prototype.getClientId = function () {
+  return this.clientid
+}
+
+Menshen.prototype.setPrivateKey = function (key) {
+  if (!key instanceof CryptoKey) { return }
+  if (key.type !== 'private') { return }
+  this.key.priv = key
+  this.kloaded = true
+}
+
+Menshen.prototype.setPublicKey = function (key) {
+  if (!(key instanceof CryptoKey)) { return }
+  if (!key.type !== 'public') { return }   
+  this.key.pub = key
+}
+
+Menshen.prototype.getPrivateKey = function () {
+  return this.key.priv
+}
+
+Menshen.prototype.getPublicKey = function () {
+  return this.key.pub
+}
+
+Menshen.prototype.getHashLength = function (h, bytes = true) {
+  switch (h.toLowerCase()) {
+    default: return -1
+    case 'sha1': case 'sha-1':
+      return bytes ? 160 >> 3 : 160
+    case 'sha256': case 'sha-256':
+      return bytes ? 256 >> 3 : 256
+    case 'sha384': case 'sha-384':
+      return bytes ? 384 >> 3 : 384
+    case 'sha512': case 'sha-512':
+      return bytes ? 512 >> 3 : 512
+  }
+}
+
+Menshen.prototype.setHash = function (h) {
+  if (this.kloaded) { console.error('Hash must be set before key is loaded'); return }
+  switch (h.toLowerCase()) {
+    case 'sha1': case 'sha-1':
+      this.hash = 'SHA-1'; break
+    case 'sha256': case 'sha-256':
+      this.hash = 'SHA-256'; break
+    case 'sha384': case 'sha-384':
+      this.hash = 'SHA-384'; break
+    case 'sha512': case 'sha-512':
+      this.hash = 'SHA-512'; break
+  }
+}
+
+Menshen.prototype.getHash = function () {
+  return this.hash
+}
+
+/* MHash : Menshen want alphanumeric lower case only */
+Menshen.prototype.getMHash = function () {
+  switch (this.getHash()) {
+    case 'SHA-1':
+      return 'sha1'
+    case 'SHA-256':
+      return 'sha256'
+    case 'SHA-384':
+      return 'sha384'
+    case 'SHA-512':
+      return 'sha512'
+  }
+}
+
+Menshen.prototype.getSaltLength = function () {
+  return this.saltLength
+}
+
+Menshen.prototype.setSaltLength = function (len) {
+  len = parseInt(len)
+  if (isNaN(len) || len === 0) { this.saltLength = 0 }
+  else {
+    /* We max salt length to digest output size */
+    if (len < 0) {
+      len = this.getHashLength(this.hash)
+    } else {
+      let hLen = this.getHashLength(this.hash)
+      if (hLen < len) { len = hLen }
+    }
+    this.saltLength = len
+  }
+}
+
+Menshen.prototype.getMID = function (method, url, reqid) {
+  return new Promise((resolve, reject) => {
+    switch (this.version) {
+      case 1:
+        resolve(`${method.trim().toLowerCase()}|${url}|${reqid.trim().toLowerCase()}`)
+        break
+      case 2:
+        crypto.subtle.importKey(
+          'raw',
+          new TextEncoder().encode(reqid),
+          {
+            name: 'HMAC',
+            hash: {name: 'SHA-256'}
+          },
+          false,
+          [ 'sign' ]
+        ).then(key => {
+          return crypto.subtle.sign(
+            {name: 'HMAC'},
+            key, 
+            new TextEncoder().encode(`${method.trim().toLowerCase()}${url}`)
+          )
+        })
+        .then(buffer => {
+          resolve(MenshenEncoding.buf2b64(buffer))
+        })
+        break
+      default:
+        reject(new Error('Version unknown'))
+        break
+    }
+  })
+}
+
+Menshen.prototype.authValues = function (clientid, method, query, qid) {
+  return new Promise((resolve, reject) => {
+    const authValues = {
+      type: this.version === 1 ? 'Menshen' : 'Menshen2',
+      cid: this.version === 1 ? clientid : MenshenEncoding.btoa(clientid),
+      sle: this.getSaltLength(),
+      dgt: this.getHash()
+    }
+    this.getMID(method, query, qid)
+    .then(mid => {
+      return MenshenRSAUtils.sign(
+        this.getPrivateKey(),
+        mid,
+        {
+          hash: authValues.hash,
+          saltLength: authValues.sle,
+          raw: true
+        }
+      )
+    })
+    .then(signed => {
+      authValues.sig = MenshenEncoding.buf2b64(signed)
+      return authValues
+    })
+    .then(authValues => {
+      resolve(authValues)
+    })
+    .catch(reason => reject(reason))
+  })
+}
+
+Menshen.prototype.qstring = function (url, method, requestId) {
+  return new Promise((resolve, reject) => {
+    this.authValues(this.clientid, method, url.host, requestId)
+    .then(authValue => {
+      url.searchParams.append('menshen_qid', requestId)
+      for(const k in authValue) {
+        url.searchParams.append(`menshen_${k}`, authValue[k])
+      }
+      resolve(url)
+    })
+  })
+}
+
+Menshen.prototype.fetch = function (url, params) {
+  return new Promise((resolve, reject) => {
+    if (!(url instanceof URL)) {
+      url = new URL(url)
+    }
+    const query = String(url.pathname + url.search)
+    let method = 'get'
+    if (params === undefined) {
+      params = {}
+    }
+    if (params.method !== undefined) {
+      method = params.method.toLowerCase()
+    }
+    if (params.headers === undefined) {
+      params.headers = new Headers()
+    }
+    if (!(params.headers instanceof Headers)) {
+      let headers = new Headers()
+      for (let k in params.headers) {
+        headers.append(k, params.headers[k])
+      }
+      params.headers = headers
+    }
+
+    let qid = ''
+    if (params.headers.has('X-Request-Id')) {
+      qid = params.headers.get('X-Request-Id')
+    } else {
+      qid =`${new Date().getTime()}-${performance.now()}`
+      params.headers.append('X-Request-ID', qid)
+    }
+    
+    this.authValues(this.clientid, method, query, qid)
+    .then(authValues => {
+      params.headers.set('Authorization', `${authValues.type} cid=${authValues.cid},sig=${authValues.sig},sle=${authValues.sle},dgt=${authValues.dgt}`)
+      return fetch(url, params)
+    })
+    .then(response => {
+      resolve(response)
+    })
+    .catch(reason => reject(reason))
+  })
+}
---
+++ php/Menshen.php
@@ -0,0 +1,421 @@
+<?PHP
+
+namespace {
+  require('phpseclib/autoload.php');
+  class Menshen {
+    protected $certStore;
+    protected $version = 1;
+    
+    function __construct($store) {
+      $this->Auth = [];
+      $this->certStore = $store;
+      $this->certStore->init();
+    }
+    
+    function check() {
+      try {
+        if (!$this->certStore->beginCheck()) { return false; }
+
+        $auth = $this->getAuth();
+        if (empty($auth)) { return false; }
+        
+        $user = $this->certStore->getUser($auth['cid']);
+
+        if (!$user->getCertificate()) { return false; }
+        $rsa = new \phpseclib\Crypt\RSA();
+
+        if(!$rsa->loadKey($user->getCertificate())) { return false; }
+
+        $mid = $this->getMID();
+        if (empty($mid)) { return false; }
+
+        $rsa->setHash($auth['dgt']);
+        $rsa->setMGFHash($auth['mgf']);
+        $rsa->setSaltLength($auth['sle']);
+        $rsa->setSignatureMode(\phpseclib\Crypt\RSA::SIGNATURE_PSS);
+
+        $success = $rsa->verify($mid, $auth['sig']);
+        if (!$this->certStore->endCheck($auth['cid'], $success)) { return false; }
+
+        if ($success) {
+          return $user;
+        }
+        return false;
+      } catch (Exception $e) {
+        error_log('Menshen::"' . $e->getMessage() . '"');
+        return false;
+      }   
+    }
+    
+    public function getCID() {
+      if (empty($this->Auth)) { return null; }
+      if (empty($this->Auth['cid'])) { return null; }
+      return $this->Auth['cid'];
+    }
+
+    public function b64decode($txt) {
+      return base64_decode(str_replace(['-', '_', '.'], ['+', '/', '='], $txt));
+    }
+
+    public function b64encode($raw) {
+      return str_replace(['+', '/', '='], ['-', '_', '.'], base64_encode($raw));
+    }
+
+    protected function getAuthValue ($name, $rawValue) {
+      switch($name) {
+        case 'qid':
+          return $rawValue;
+        case 'cid':
+          switch($this->version) {
+            default: return null;
+            case 1:
+              return $rawValue;
+            case 2:
+              return $this->b64decode($rawValue);
+          }
+          break;
+        case 'sle':
+          return intval($rawValue);
+        case 'sig':
+          switch($this->version) {
+            default: return null;
+            case 1:
+              return hex2bin($rawValue);
+            case 2:
+              return $this->b64decode($rawValue);
+          }
+          break;
+        case 'mgf':
+        case 'dgt':
+          $rawValue = strtolower($rawValue);
+          switch ($rawValue) {
+            case 'md2':
+            case 'md5':
+            case 'sha1':
+            case 'sha256':
+            case 'sha384':
+            case 'sha512':
+              return $rawValue;
+            case 'sha-256':
+              return 'sha256';
+            case 'sha-384':
+              return 'sha384';
+            case 'sha-512':
+              return 'sha512';
+          }
+        default: return null;
+      }
+    }
+
+    protected function getQSAuth () {
+      $args = [
+        'cid' => false, /* client id */
+        'sig' => false, /* signature */
+        'dgt' => 'sha256', /* digest */
+        'sle' => 0, /* saltlen */
+        'mgf' => 'sha256',
+        'sig' => false,
+        'qid' => false
+      ];
+
+      if (!isset($_REQUEST['menshen_type'])) { return[]; }
+
+      switch(strtolower($_REQUEST['menshen_type'])) {
+        case 'menshen': $this->version = 1; break;
+        case 'menshen2': $this->version = 2; break;
+        default: return [];
+      }
+      
+      foreach ($_REQUEST as $k => $v) {
+        if (strpos($k, 'menshen_') === 0) {
+          $k = substr($k, 8);
+          if ($k === 'type') { continue; }
+          if (isset($args[$k])) {
+            $args[$k] = $this->getAuthValue($k, $v);
+            if ($args[$k] === null) { return []; }
+          }
+        }
+      }
+
+      if ($args['cid'] === false || $args['sig'] === false) { return []; }
+      $this->Auth = $args;
+      return $args;
+    }
+
+    protected function getAuth () {
+      if (!empty($this->Auth)) { return $this->Auth; }
+
+      $args = [
+        'cid' => false, /* client id */
+        'sig' => false, /* signature */
+        'dgt' => 'sha256', /* digest */
+        'sle' => 0, /* saltlen */
+        'mgf' => 'sha256',
+        'sig' => false
+      ];
+
+      if (empty($_SERVER['HTTP_AUTHORIZATION'])) { return $this->getQSAuth(); }
+
+      $version = 1;
+      $auth = trim($_SERVER['HTTP_AUTHORIZATION']);
+      if (strncasecmp($auth, 'menshen', 7) !== 0) { return []; }
+      if ($auth[7] === '2') { $version = 2; }
+      $this->version = $version; 
+
+      $authstr = explode(',', substr($auth, $version === 1 ? 7 : 8));
+
+      foreach ($authstr as $p) {
+        if (strpos($p, '=') === -1) { return[]; }
+        list ($k, $v) = explode('=', $p, 2);
+        $k = trim($k);
+        $v = trim($v);
+        $args[$k] = $this->getAuthValue($k, $v);
+        if ($args[$k] === null) { return []; }
+      }
+      if ($args['cid'] === false || $args['sig'] === false) { return []; }
+      $this->Auth = $args;
+      return $args;
+    }
+    
+    public function getMID () {
+      if (
+        empty($_SERVER['REQUEST_METHOD']) || 
+        empty(isset($this->Auth['qid']) ? $this->Auth['qid'] : $_SERVER['HTTP_X_REQUEST_ID']) ||
+        empty($_SERVER['REQUEST_URI'])
+      ) {
+        return '';
+      }
+
+      switch ($this->version) {
+        case 1:
+          return sprintf(
+            '%s|%s|%s',
+            strtolower(trim($_SERVER['REQUEST_METHOD'])),
+            isset($this->Auth['qid']) ? $_SERVER['HTTP_HOST'] : $_SERVER['REQUEST_URI'],
+            strtolower(trim(isset($this->Auth['qid']) ? $this->Auth['qid'] : $_SERVER['HTTP_X_REQUEST_ID']))
+          );
+        case 2:
+          return $this->b64encode(
+            hash_hmac(
+              'sha256',
+              strtolower(trim($_SERVER['REQUEST_METHOD'])) . (isset($this->Auth['qid']) ? $_SERVER['HTTP_HOST'] : $_SERVER['REQUEST_URI']),
+              isset($this->Auth['qid']) ? $this->Auth['qid'] : $_SERVER['HTTP_X_REQUEST_ID'],
+            true)
+          );
+      }
+      return '';
+    }
+  }
+}
+
+namespace Menshen {
+  interface CertStore {
+    public function init();
+    public function beginCheck();
+    public function endCheck($clientId, $success);
+    public function getUser($clientId);
+  }
+
+  class GenericUser {
+    private $displayName = null;
+    private $dbId = null;
+    private $uid = null;
+    private $certificate = null;
+
+    function __construct($init = []) {
+      foreach($init as $k => $v) {
+        switch ($k) {
+          case 'dbid': $this->setDbId($v); break;
+          case 'uid': $this->setUid($v); break;
+          case 'displayname': $this->setDisplayName($v); break;
+          case 'certificate': $this->setCertificate($v); break;
+        }
+      }
+    }
+
+    function setDisplayName($name) {
+      $this->displayName = $name;
+    }
+    function setDbId($id) {
+      $this->dbId = $id;
+    }
+    function setUid($id) {
+      $this->uid = $id;
+    }
+    function setCertificate($certificate) {
+      $this->certificate = $certificate;
+    }
+    function getDisplayName($name) {
+      return  $this->displayName;
+    }
+    function getDbId($id) {
+      return $this->dbId;
+    }
+    function getUid($id) {
+      return $this->uid;
+    }
+    function getCertificate() {
+      return $this->certificate;
+    }
+  }
+
+  class FILEStore implements CertStore {
+    protected $path;
+    function __construct ($path) {
+      if (!is_dir($path) || !is_readable($path)) {
+        throw new \Exception('Path is not a dir or readable "' . $path . '"');
+      }
+    }
+    function init() { return true; }
+
+    function beginCheck() { return true; }
+
+    function endCheck($clientId, $success) { return true; }
+
+    function getUser($clientId) {
+      $fullpath = $this->path . '/' . $clientId . '.pem';
+      $person = [
+        'uid' => $clientId,
+        'certificate' => null
+      ];
+      if (is_readable($fullpath)) {
+        $pem = file_get_contents($fullpath);
+        if ($pem) {
+          $person['certificate'] = $pem;
+        }
+      }
+      return new GenericUser($person);
+    }
+  }
+
+  /* == DB ==
+     CREATE TABLE IF NOT EXISTS "menshen" ("clientid" INTEGER PRIMARY KEY AUTO_INCREMENT, "pkcs8" TEXT NOT NULL);
+   */  
+  class PDOStore implements CertStore {
+    protected $db;
+    protected $tname;
+    
+    function __construct($pdoConn, $tableName) {
+      $this->db = $pdoConn;
+      $this->tname = $tableName;
+    }
+
+    function init() { return true; }
+
+    function beginCheck() { return true; }
+
+    function endCheck($clientId, $success) { return true; }
+
+    function getUser($clientId) {
+      $st = $this->db->prepare('SELECT * FROM "' . $this->tname . '" WHERE "clientid" = :cid');
+      $st->bindParam(':cid', $clientId, \PDO::PARAM_INT);
+      $person = [
+        'uid' => $clientId,
+        'certificate' => null
+      ];
+      if ($st->execute()) {
+        if (($row = $st->fetch()) !== false) {
+          $person['certificate'] = $row['pkcs8'];
+        }
+      }
+
+      return new GenericUser($person);
+    }  
+  }
+
+  class LDAPStore implements CertStore {
+    protected $conn;
+    protected $base;
+    
+    function __construct($ldap, $base) {
+      $this->conn = $ldap;
+      $this->base = $base;
+    }
+
+    function init() { return true; }
+    function beginCheck() { return true; }
+    function endCheck($clientId, $success) { return true; }
+    function getUser($clientId) {
+      $fclientId = ldap_escape($clientId, '', LDAP_ESCAPE_FILTER);
+      $filter = sprintf('(|(uid=%s)(x500uniqueIdentifier=%s)(cn=%s)(mail=%s)(sn=%s))', $fclientId, $fclientId, $fclientId, $fclientId, $fclientId);
+      $res = @ldap_search(
+        $this->conn,
+        $this->base,
+        $filter,
+        [ 'sn', 'mail', 'uid', 'x500uniqueIdentifier', 'cn', 'userCertificate', 'userSMIMECertificate', 'userPKCS12', 'displayName' ],
+        0,
+        1
+      );
+      if (!$res) { return false; }
+      $entry = @ldap_first_entry($this->conn, $res);
+      if (!$entry) { return false; }
+      $person = [
+        'certificate' => null,
+        'displayname' => null,
+        'uid' => null,
+        'dbid' => @ldap_get_dn($this->conn, $entry)
+      ];
+      if (!$person['dbid']) { return false; }
+
+      for ($attr = @ldap_first_attribute($this->conn, $entry); $attr; $attr = @ldap_next_attribute($this->conn, $entry)) {
+        switch(strtolower($attr)) {
+          case 'usercertificate':
+            $val = @ldap_get_values_len($this->conn, $entry, $attr);
+            if (!$val) { break; }
+            if ($val['count'] > 0) {
+              $person['certificate'] = $val[0];
+            }
+            break;
+          case 'usersmimecertificate':
+          case 'userpkcs12':
+            /* userCertificate is prefered and set anyway */
+            if ($person['certificate']) { break; }
+            $val = @ldap_get_values($this->conn, $entry, $attr);
+            if (!$val) { break; }
+            if ($val['count'] > 0) {
+              $person['certificate'] = $val[0];
+            }
+            break;
+          case 'uid':
+            $val = @ldap_get_values($this->conn, $entry, $attr);
+            if (!$val) { break; }
+            if ($val['count'] > 0) {
+              $person['uid'] = $val[0];
+            }
+            break;
+          case 'sn':
+          case 'mail':
+          case 'cn':
+            $val = @ldap_get_values($this->conn, $entry, $attr);
+            if (!$val) { break; }
+            if ($val['count'] > 0) {
+              if (!$person['uid']) {
+                $person['uid'] = $val[0];
+              }
+              if (!$person['displayname']) {
+                $person['displayname'] = $val[0];
+              }
+            }
+            break;
+          case 'x500uniqueidentifier':
+            if ($person['uid']) { break; }
+            $val = @ldap_get_values_len($this->conn, $entry, $attr);
+            if (!$val) { break; }
+            if ($val['count'] > 0) {
+              $person['uid'] = $val[0];
+            }
+            break;
+          case 'displayname':
+            $val = @ldap_get_values($this->conn, $entry, $attr);
+            if (!$val) { break; }
+            if ($val['count'] > 0) {
+              $person['displayname'] = $val[0];
+            }
+            break;
+        }
+      }
+      return new GenericUser($person);
+    }
+  }
+}
+?>
